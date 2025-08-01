1f56d6f232c034b4ce606d23a208dbfecf0c80ac
Greg Christiana <maxuser@pyjinn.org>
2025-07-31 23:13:57 -0400

    Remove java.vendor, build.timestamp from version
    Java vendor shouldn't matter. And build timestamp was not updating
    properly, and would make the build non-deterministic if it did.

--- interpreter/src/main/java/org/pyjinn/interpreter/VersionInfo.java
+++ interpreter/src/main/java/org/pyjinn/interpreter/VersionInfo.java
@@ -6,27 +6,21 @@ package org.pyjinn.interpreter;
 import java.io.InputStream;
 import java.util.Properties;
 
-public record VersionInfo(
-    String pyjinnVersion, String buildTimestamp, String javaVendor, String javaVersion) {
+public record VersionInfo(String pyjinnVersion, String javaVersion) {
 
   static VersionInfo load() throws Exception {
     Properties prop = new Properties();
     try (InputStream input =
         VersionInfo.class.getClassLoader().getResourceAsStream("build.properties")) {
       if (input == null) {
-        return new VersionInfo("?", "?", "?", "?");
+        return new VersionInfo("?", "?");
       }
       prop.load(input);
-      return new VersionInfo(
-          prop.getProperty("pyjinn.version"),
-          prop.getProperty("build.timestamp"),
-          prop.getProperty("java.vendor"),
-          prop.getProperty("java.version"));
+      return new VersionInfo(prop.getProperty("pyjinn.version"), prop.getProperty("java.version"));
     }
   }
 
   public String toString() {
-    return "Pyjinn %s (%s) [%s Java %s]"
-        .formatted(pyjinnVersion, buildTimestamp, javaVendor, javaVersion);
+    return "Pyjinn %s [Java %s]".formatted(pyjinnVersion, javaVersion);
   }
 }
