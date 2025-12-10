c450268202f0c5e64d23bce053139f9ed0aa4d1c
Greg Christiana <maxuser@pyjinn.org>
2025-12-09 23:41:36 -0500

    Support compiled WhileBlock
    Also support `break` and `continue` statements within loops.

--- interpreter/src/main/java/org/pyjinn/interpreter/Compiler.java
+++ interpreter/src/main/java/org/pyjinn/interpreter/Compiler.java
@@ -3,16 +3,66 @@
 
 package org.pyjinn.interpreter;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
+import java.util.function.Function;
 import org.pyjinn.interpreter.Script.*;
 
 class Compiler {
   public static void compile(Statement statement, List<Instruction> instructions) {
-    compileStatement(statement, instructions);
+    var compiler = new Compiler();
+    compiler.compileStatement(statement, instructions);
   }
 
-  private static void compileStatement(Statement statement, List<Instruction> instructions) {
+  private static class LoopState {
+    private final List<Instruction> instructions;
+    public final int continueTarget;
+    private final ArrayList<BreakInstruction> breakInstructions;
+
+    private record BreakInstruction(int source, Function<Integer, Instruction> instructor) {}
+
+    public LoopState(List<Instruction> instructions) {
+      this.instructions = instructions;
+      continueTarget = instructions.size();
+      breakInstructions = new ArrayList<>();
+    }
+
+    public void addBreak(Function<Integer, Instruction> breakInstruction) {
+      int breakIp = instructions.size();
+      instructions.add(null);
+      breakInstructions.add(new BreakInstruction(breakIp, breakInstruction));
+    }
+
+    public void close(int breakTarget) {
+      for (var breakInstruction : breakInstructions) {
+        instructions.set(breakInstruction.source, breakInstruction.instructor.apply(breakTarget));
+      }
+    }
+  }
+
+  private final Deque<LoopState> loops = new ArrayDeque<>();
+
+  private LoopState loop() {
+    return loops.peek();
+  }
+
+  private void addBreak(Function<Integer, Instruction> breakInstruction) {
+    if (loops.isEmpty()) {
+      throw new IllegalStateException("break statement outside of loop");
+    }
+    loop().addBreak(breakInstruction);
+  }
+
+  private int continueTarget() {
+    if (loops.isEmpty()) {
+      throw new IllegalStateException("continue statement outside of loop");
+    }
+    return loop().continueTarget;
+  }
+
+  private void compileStatement(Statement statement, List<Instruction> instructions) {
     if (statement instanceof StatementBlock block) {
       for (var s : block.statements()) {
         compileStatement(s, instructions);
@@ -24,8 +74,14 @@ class Compiler {
       compileAssignment(assign, instructions);
     } else if (statement instanceof IfBlock ifBlock) {
       compileIfBlock(ifBlock, instructions);
+    } else if (statement instanceof WhileBlock whileBlock) {
+      compileWhileBlock(whileBlock, instructions);
     } else if (statement instanceof FunctionDef function) {
       compileFunctionDef(function, instructions);
+    } else if (statement instanceof Continue continueStatement) {
+      compileContinueStatement(continueStatement, instructions);
+    } else if (statement instanceof Break breakStatement) {
+      compileBreakStatement(breakStatement, instructions);
     } else if (statement instanceof ReturnStatement returnStatement) {
       compileReturnStatement(returnStatement, instructions);
     } else {
@@ -33,7 +89,7 @@ class Compiler {
     }
   }
 
-  private static void compileAssignment(Assignment assign, List<Instruction> instructions) {
+  private void compileAssignment(Assignment assign, List<Instruction> instructions) {
     Expression lhs = assign.lhs();
     if (lhs instanceof Identifier identifier) {
       compileExpression(assign.rhs(), instructions);
@@ -48,7 +104,7 @@ class Compiler {
     }
   }
 
-  private static void compileIfBlock(IfBlock ifBlock, List<Instruction> instructions) {
+  private void compileIfBlock(IfBlock ifBlock, List<Instruction> instructions) {
     // if CONDITION:
     //   THEN_BODY
     //
@@ -94,7 +150,29 @@ class Compiler {
     }
   }
 
-  private static void compileFunctionDef(FunctionDef function, List<Instruction> instructions) {
+  private void compileWhileBlock(WhileBlock whileBlock, List<Instruction> instructions) {
+    // while CONDITION:
+    //   BODY
+    //
+    // compiles to instructions:
+    //   [0] eval CONDITION
+    //   [1] JumpIfFalse 4  # breakLoopSource = 1, breakLoopTarget = 4
+    //   [2] execute BODY
+    //   [3] Jump 0  # continueLoopSource = 3, continueLoopTarget = 0
+    //   [4] ...
+    //
+    // `continue` in BODY -> Jump 0
+    // `break` in BODY -> Jump 4
+
+    loops.push(new LoopState(instructions));
+    compileExpression(whileBlock.condition(), instructions);
+    addBreak(ip -> new Instruction.JumpIfFalse(ip));
+    compileStatement(whileBlock.body(), instructions);
+    instructions.add(new Instruction.Jump(continueTarget()));
+    loops.pop().close(instructions.size());
+  }
+
+  private void compileFunctionDef(FunctionDef function, List<Instruction> instructions) {
     var functionInstructions = new ArrayList<Instruction>();
     compile(function.body(), functionInstructions);
 
@@ -106,13 +184,22 @@ class Compiler {
     instructions.add(new Instruction.BindFunction(function, functionInstructions));
   }
 
-  private static void compileReturnStatement(
+  private void compileContinueStatement(
+      Continue continueStatement, List<Instruction> instructions) {
+    instructions.add(new Instruction.Jump(continueTarget()));
+  }
+
+  private void compileBreakStatement(Break breakStatement, List<Instruction> instructions) {
+    addBreak(ip -> new Instruction.Jump(ip));
+  }
+
+  private void compileReturnStatement(
       ReturnStatement returnStatement, List<Instruction> instructions) {
     compileExpression(returnStatement.returnValue(), instructions);
     instructions.add(new Instruction.FunctionReturn());
   }
 
-  private static void compileFunctionCall(FunctionCall call, List<Instruction> instructions) {
+  private void compileFunctionCall(FunctionCall call, List<Instruction> instructions) {
     compileExpression(call.method(), instructions);
     int numArgs = 0;
     for (var kwarg : call.kwargs().reversed()) {
@@ -126,7 +213,7 @@ class Compiler {
     instructions.add(new Instruction.FunctionCall(call.filename(), call.lineno(), numArgs));
   }
 
-  private static void compileExpression(Expression expr, List<Instruction> instructions) {
+  private void compileExpression(Expression expr, List<Instruction> instructions) {
     if (expr instanceof Identifier identifier) {
       instructions.add(new Instruction.Identifier(identifier.name()));
     } else if (expr instanceof FunctionCall call) {
